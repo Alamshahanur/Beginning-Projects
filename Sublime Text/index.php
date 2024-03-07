1a1983ed7cf599a36c17279423bb1dd7c1e8cb54
Shahanur <alamshahanur2002@gmail.com>
2024-03-07 23:30:13 +0530

    first commit

---
+++ Sublime Text/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PhP first practice</title>
+</head>
+<body>
+    <div id="PhP">
+        This is my first php practice
+        <?php
+        //This is single line comment
+        echo " Hellow world this is php practice so please like and follow for more content" ;
+        ?>
+    </div>
+</body>
+</html>
